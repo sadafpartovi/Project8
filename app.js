163350672379497e94a1fa003fd4d55db62e1cf9
sadafpartovi <74632037+sadafpartovi@users.noreply.github.com>
2021-01-19 16:26:24 -0800

    adding final touches

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let employees = [];
-const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
+const urlAPI =`https://randomuser.me/api/?results=12&inc=name, picture,
 email, location, phone, dob &noinfo &nat=US`
 const gridContainer = document.querySelector(".grid-container");
 const overlay = document.querySelector(".overlay");
@@ -57,7 +57,7 @@ fetch(urlAPI)
             <p class="address">${city}</p>
             <hr>
             <p>${phone}</p>
-            <p class="address">${street}, ${state} ${postcode}</p>
+            <p class="address">${street.number} ${street.name}, ${state} ${postcode}</p>
             <p>Birthday:
             ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
         </div>
